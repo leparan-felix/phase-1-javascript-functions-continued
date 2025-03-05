8cf8ba1e4705ee6b6475a1c90b203693adafe516
leparan <leparanfelix16@gmail.com>
2025-03-05 12:16:38 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  console.log(saturdayFun()); // "This Saturday, I want to roller-skate!"
+console.log(saturdayFun("watch movies")); // "This Saturday, I want to watch movies!"
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  console.log(mondayWork()); // "This Monday, I will go to the office."
+  console.log(mondayWork("work from home")); // "This Monday, I will work from home."
+  function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+
+  const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction("a dedicated programmer"));
+// "You are !!!a dedicated programmer!!!"
+
+console.log(wrapAdjective("%")("an awesome coder"));
+// "You are %an awesome coder%!"
+
